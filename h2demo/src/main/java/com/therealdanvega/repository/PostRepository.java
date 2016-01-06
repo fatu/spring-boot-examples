f86448c91bf3ef762de79ddd26c7aa06f14982ed
fatu <fatu@ebay.com>
2016-01-06 08:58:20 +0800

    h2 data insert.

---
+++ h2demo/src/main/java/com/therealdanvega/repository/PostRepository.java
@@ -0,0 +1,13 @@
+package com.therealdanvega.repository;
+
+import com.therealdanvega.domain.Post;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author boaztu
+ */
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+
+}
